b39809ad739019e8c299c0cb12034c3c7d0e3188
Szabó Ákos Csaba <szabo.akos.csaba@blathy.info>
2024-10-15 15:23:09 +0200

    Keret + Rajz program egyesítése (spacebar még nincs megoldva) -> Create működik

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 ﻿class Program
+
 {
+    static int cursorX = 1;
+    static int cursorY = 1;
+    static char currentDrawingChar = '█';
+    static ConsoleColor currentColor = ConsoleColor.White;
+
     static void Main()
     {
         string[] opciok = { "Create", "Edit", "Delete", "Escape" };
@@ -33,13 +39,129 @@
                     if (aktKivalasztas < opciok.Length - 1)
                         aktKivalasztas++;
                     break;
+
                 case ConsoleKey.Enter:
-                    MenuRajzolas(opciok, aktKivalasztas, opciok[aktKivalasztas]);
+                    if (opciok[aktKivalasztas] == "Create")
+                    {
+
+                        StartDrawingTool();
+                    }
+                    else if (opciok[aktKivalasztas] == "Escape")
+                    {
+                        key = ConsoleKey.Escape;
+                    }
                     enter = true;
                     break;
             }
         }
         while (key != ConsoleKey.Escape);
+    }
+
+    static void StartDrawingTool()
+    {
+        cursorX = 1;
+        cursorY = 1;
+        currentDrawingChar = '█';
+        currentColor = ConsoleColor.White;
+
+        DrawFrame();
+
+        
+        bool exitDrawing = false;
+
+
+        while (!exitDrawing)
+        {
+            if (Console.KeyAvailable)
+            {
+                var keyInfo = Console.ReadKey(true);
+
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (cursorY > 1) cursorY--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (cursorY < Console.WindowHeight - 2) cursorY++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (cursorX > 1) cursorX--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (cursorX < Console.WindowWidth - 2) cursorX++;
+                        break;
+
+                    case ConsoleKey.Escape:
+                        exitDrawing = true;
+                        break;
+
+
+                    case ConsoleKey.D1:
+                        currentDrawingChar = '█';
+                        break;
+                    case ConsoleKey.D2:
+                        currentDrawingChar = '▓';
+                        break;
+                    case ConsoleKey.D3:
+                        currentDrawingChar = '▒';
+                        break;
+                    case ConsoleKey.D4:
+                        currentDrawingChar = '░';
+                        break;
+
+
+                    case ConsoleKey.D5:
+                        currentColor = ConsoleColor.Red;
+                        break;
+                    case ConsoleKey.D6:
+                        currentColor = ConsoleColor.Blue;
+                        break;
+                    case ConsoleKey.D7:
+                        currentColor = ConsoleColor.Green;
+                        break;
+                    case ConsoleKey.D8:
+                        currentColor = ConsoleColor.Yellow;
+                        break;
+                    case ConsoleKey.D9:
+                        currentColor = ConsoleColor.Cyan;
+                        break;
+                }
+            }
+        }
+    }
+    static void DrawFrame()
+    {
+        Console.Clear();
+
+
+        Console.SetCursorPosition(0, 0);
+        Console.Write("╔");
+        for (int i = 1; i < Console.WindowWidth - 1; i++)
+        {
+            Console.Write("═");
+        }
+        Console.Write("╗");
+
+
+        for (int i = 1; i < Console.WindowHeight - 1; i++)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.Write("║");
+            Console.SetCursorPosition(Console.WindowWidth - 1, i);
+            Console.Write("║");
+        }
+
+
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        Console.Write("╚");
+        for (int i = 1; i < Console.WindowWidth - 1; i++)
+        {
+            Console.Write("═");
+        }
+        Console.Write("╝");
+
+        Console.ResetColor();
     }
 
     static void MenuRajzolas(string[] opciok, int selectedOption, string kivalasztottSzoveg)
